147b218c923ce6d2732ac8c497146e9e65d9bf77
Manasses-Alberto <manassesndombele88@gmail.com>
2023-12-22 13:08:32 +0100

    Criei todos os estilos do modal de captura tanto para mobile como para desktop

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-document.getElementById('buy-ebooks-btn').addEventListener('click', function () {
+document.querySelector('#buy-ebooks-btn').addEventListener('click', function () {
     document.querySelector('header').style.opacity = '0.1'
     document.querySelector('header').style.pointerEvents = 'none'
     document.querySelector('main').style.opacity = '0.1'
@@ -7,4 +7,15 @@ document.getElementById('buy-ebooks-btn').addEventListener('click', function () 
     document.querySelector('footer').style.pointerEvents = 'none'
     document.body.style.overflowY = 'hidden'
     document.getElementById('modal-squeeze').classList.remove('visually-hidden')
+})
+
+document.querySelector('#close-modal-btn').addEventListener('click', () => {
+    document.getElementById('modal-squeeze').classList.add('visually-hidden')
+    document.querySelector('header').style.opacity = '1'
+    document.querySelector('header').style.pointerEvents = 'auto'
+    document.querySelector('main').style.opacity = '1'
+    document.querySelector('main').style.pointerEvents = 'auto'
+    document.querySelector('footer').style.opacity = '1'
+    document.querySelector('footer').style.pointerEvents = 'auto'
+    document.body.style.overflowY = 'auto'
 })
